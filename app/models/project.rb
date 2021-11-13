04b2c7a4c0f3cd91badd3f9edafc6c23bb6674b8
dajameson <djameson@uccs.edu>
2021-11-13 23:49:53 +0000

    comment out project.rb code for CI homework 6

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,3 @@
 class Project < ApplicationRecord
-    validates_presence_of :title, :description #makes sure that the presense of a title and description exists, fails if they do not exist
+   # validates_presence_of :title, :description #makes sure that the presense of a title and description exists, fails if they do not exist
 end
